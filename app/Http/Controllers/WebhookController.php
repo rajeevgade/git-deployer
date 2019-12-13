d763e4bff4c398551beb7c457b8400e9b0646fa4
rajeevgade <rajeevgade@gmail.com>
2019-12-13 16:01:47 -0600

    --changes to controller

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -8,8 +8,50 @@ class WebhookController extends BaseController
 {
     public function index(Request $request){
 
-        $content = file_get_contents("php://input");
+        $startTime = microtime(true);
 
-        return $this->sendResponse(json_encode($content));
+        $headers = $request->headers->all();
+
+        $postData = $request->getContent();
+
+        $bodyData = file_get_contents('php://input');
+
+        $validate = $this->validateData($bodyData);
+
+       if(!$validate){
+           return $this->sendError("No Valid Handler Found");
+       }
+
+       $endTime = microtime(true);
+
+       $message = "Repo <repo-name> - <branch-name> synced in " .number_format($endTime - $startTime, 2) . "seconds";
+        
+       $this->updateLog($message);
+
+        return $this->sendResponse($message);
+    }
+
+    public function validateData(){
+        return true;
+    }
+
+    public function updateLog($msg = ""){
+
+        $log_file = '/logs/git.log';
+        
+        $content = "===================";
+        $content .= $msg;
+        $content .= "===================";
+        
+        //open file and write log message
+        if (!file_exists($log_file)){
+			file_put_contents($log_file, $content);
+		}
+        else{
+			$fp = fopen($log_file, 'a');
+			fwrite($fp, $content);  
+			fclose($fp);  
+		}
+		
     }
 }
